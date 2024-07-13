4d243244f59c3036e9440eb789539be18f3f1032
ritu-477 <ritujoharbishnoi@gmail.com>
2024-07-13 14:00:54 +0530

    js-practice

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+// ...................arithmetic operators//
+// addition
+let a = 2;
+console.log(a + 3);
+
+// Subtract
+let b = 5;
+console.log(b - 3);
+
+// Multiply
+let c = 5;
+console.log(c * 3);
+
+// Divide
+let d = 12;
+console.log(d / 6);
+
+// reminder
+let e = 12;
+console.log(e % 8);
+
+// Exponential
+let f = 12;
+console.log(f ** 2);
+
+// ...........Assignment.............//
+// assign
+let g = 10;
+console.log(g = 8);
+
+//Add then assign
+let h = 10;
+console.log(h += 8);
+
+// Subtract then assign
+let i = 12;
+console.log(i -= 8);
+
+// Multiply then assign
+let j = 12;
+console.log(j *= 8);
+
+// .......................logical.....//
+// .//or
+const k = undefined;
+const l = 8;
+console.log(k || l)
+
+// && and
+let x = 17;
+let y = 16;
+console.log(x < y)
+
+let isR = true;
+let isS = true;
+
+if (isR && isS) {
+    console.log("Perfect weather for outdoor activities!");
+} else {
+    console.log("Maybe another day.");
+}
+
+
